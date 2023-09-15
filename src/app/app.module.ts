32d8a088c3858ab98e4d285b1583a00091b83a1e
Mau03gm <gatojs0109@outlook.com>
2023-09-15 00:48:08 -0600

    certificates component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CertificatesComponent } from './components/certificates/certificates.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CertificatesComponent
   ],
   imports: [
     BrowserModule,
